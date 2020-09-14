8e1b593e310bd2e7de42407d0df1e9c201466b2a
Djandrew10 <adrcristian@gmail.com>
2020-09-14 13:45:56 -0400

    testNGFramework

---
+++ src/test/java/com/hrms/testbase/BaseClass.java
@@ -0,0 +1,65 @@
+package com.hrms.testbase;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.hrms.utils.ConfigsReader;
+import com.hrms.utils.Constants;
+
+public class BaseClass {
+	
+	protected static WebDriver driver; //or public	
+	/*
+	* @method = to open the browser and to wait for it to be loaded and refresh it
+	* it uses the method readProperties(); from the ConfigsReader class; and reads CONFIGURATION_FILEPATH file from Constants class
+	* By calling method getPropValue(key); from the ConfigsReader class; we are getting the value of the key from configuration.properties file in the folder configs
+	* by passing the CONFIGURATION_FILEPATH from Constants class(utils)
+	*/
+	@BeforeMethod(alwaysRun=true)
+	public void setUp() {
+		
+		ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+		
+		switch (ConfigsReader.getPropValue("browser").toLowerCase()) {
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver",Constants.CHROMEDRIVER_PATH );
+		driver = new ChromeDriver();
+		break;
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", Constants.GECKODRIVER_PATH);
+		driver = new FirefoxDriver();
+		break;
+		case "edge":
+			System.setProperty("webdriver.edge.driver",Constants.EDGE_PATH );
+			driver = new FirefoxDriver();
+			break;
+		default:
+			throw new RuntimeException("Browser is not supported"); //if pass some wrong value it will throw an exception
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Constants.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
+		driver.navigate().to(ConfigsReader.getPropValue("applicationUrl"));
+		driver.navigate().refresh();
+		PageInitializer.initializePageObjects(); //calling the method initializePageObjects() from the PageInitializer class(testbase), to initialize all the classes from (pages)
+	}
+	
+	/*
+	* @method - to quit the browser, it quits only if the page is open
+	*/
+	@AfterMethod(alwaysRun=true)
+	public void tearDown() {
+		if(driver != null) {
+			driver.quit();
+			
+			
+		}
+	}
+
+}
---
+++ src/test/java/com/hrms/testcases/AddEmployeeTest.java
@@ -0,0 +1,100 @@
+package com.hrms.testcases;
+
+import java.util.List;
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.hrms.utils.CommonMethods;
+import com.hrms.utils.ConfigsReader;
+import com.hrms.utils.Constants;
+import com.hrms.utils.ExcelUtility;
+
+public class AddEmployeeTest extends CommonMethods{
+
+	@DataProvider 
+	public Object[][] employeesData() {
+		Object[][] data = { { "AnnaReaw", "AnneS12345)*)", "Annituaw", "Smitowl" },//next time the username can be changed only in order to add another employees on the web site
+				{ "AndreiYeyw", "AndrewL123(**s", "Bibberrt", "Lockssrul" },
+				{ "Benjaabee", "Ben12#%&*(*", "Benitro", "Mathewor" },
+				{ "BeyorRkey", "BeyonceC23#$*)$", "Chabbythro&", "CarRorr" },
+				{ "Jeseptee", "JessieR013#%*))", "EmilLrf", "Rodrigqorm" } };
+		return data;
+	}
+
+	@Test (groups="regression",dataProvider="employeesData")
+	public void addEmployeesData(String usernameText, String passwordText, String firstnameText, String lastnameText) {
+		sendText(login.userNametextBox, ConfigsReader.getPropValue("username")); 
+		sendText(login.passwordTextBox, ConfigsReader.getPropValue("password"));
+		click(login.btnLogin);
+		
+	//	click(dash.pimLinkBtn);
+	//	waitForVisibility(dash.addEmployeeBtn);
+	//	click(dash.addEmployeeBtn);
+		
+		//or javascript method to be sure it clicks
+		jsClick(dash.pimLinkBtn);
+		waitForVisibility(dash.addEmployeeBtn);
+		jsClick(dash.addEmployeeBtn);
+		
+		sendText(addEmp.firstNameField, firstnameText);
+		sendText(addEmp.lastName,lastnameText);
+		click(addEmp.createLoginDetailsCheckbox);
+		sendText(addEmp.userName,usernameText);
+		sendText(addEmp.userPassword,passwordText);
+		sendText(addEmp.confirmPassword,passwordText);
+		click(addEmp.saveButton);
+		waitForVisibility(persDetails.progilePicText);
+		Assert.assertEquals(persDetails.progilePicText.getText(), firstnameText + " "+ lastnameText ); //validation of employee
+	//	System.out.println(perDetails.progilePicText.getText());
+	}
+	
+	@Test(groups="regression")
+	public void addMultipleEmployees(){
+		sendText(login.userNametextBox, ConfigsReader.getPropValue("username"));
+		sendText(login.passwordTextBox, ConfigsReader.getPropValue("password"));
+		click(login.btnLogin);
+
+	click(dash.pimLinkBtn);
+	waitForVisibility(dash.addEmployeeBtn);
+	click(dash.addEmployeeBtn);
+	//	jsClick(dash.pimLinkBtn);
+	//	jsClick(dash.addEmployeeBtn);
+   
+   /*
+    * excelToListMap() method from ExcelUtility class(utils) with parameters: filepath-> TESTDATA_FILEPATH from Constants class(utils);
+    * and parameter: name of the sheet we need from the excel file "AddEmployee"
+    * gets data and stores in list of maps
+    * 
+    */
+	
+		List<Map<String, String>> employeeList = ExcelUtility.excelToListMap(Constants.TESTDATA_FILEPATH,
+				"AddEmployee");
+		SoftAssert soft = new SoftAssert();   // in order to be outside of the loop
+		for (Map<String, String> map : employeeList) {
+		//	waitForVisibility(dash.addEmployeeBtn);
+		//	jsClick(dash.addEmployeeBtn);     //to click on add employee button for the new employee
+		    waitForVisibility(dash.addEmployeeBtn);
+			click(dash.addEmployeeBtn);
+			String firstName = map.get("FirstName");
+			String middleName = map.get("MiddleName");
+			String lastName = map.get("LastName");
+            
+			waitForVisibility(addEmp.firstNameField);
+			sendText(addEmp.firstNameField, firstName);
+			waitForVisibility(addEmp.middleName);
+			sendText(addEmp.middleName, middleName);
+			waitForVisibility(addEmp.lastName);
+			sendText(addEmp.lastName, lastName);
+			jsClick(addEmp.saveButton);
+			waitForVisibility(persDetails.progilePicText);
+			soft.assertEquals(persDetails.progilePicText.getText(), firstName + " " + middleName + " " + lastName); //validation of employee
+	      CommonMethods.takeScreenshot("addMultipleEmployees");
+		}
+		soft.assertAll();   //outside of the loop 
+	}
+	
+}
